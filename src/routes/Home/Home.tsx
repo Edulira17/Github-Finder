ffabd17e535c1f38ec277e6569c778c3b9e26f25
Eduardo Lira <eduardoliracunha17@gmail.com>
2024-11-28 10:22:01 -0400

    style: component user

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,6 +1,6 @@
 import Search from "../../components/Search/Search";
 import User from "../../components/User/User";
-
+import Error from "../../components/Error/Error";
 
 import { UserProps } from "../../types/user";
 
@@ -8,31 +8,40 @@ import { useState } from "react";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
 
   const loaderUser = async (userName: string) => {
+    setError(false)
+    setUser(null)
+
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
-    
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
+
     // resgatando apenas dados que vou precisar:
-    const {avatar_url, login, location, followers, following} = data;
+    const { avatar_url, login, location, followers, following } = data;
 
     const userData: UserProps = {
       avatar_url,
       login,
       location,
       followers,
-      following
+      following,
     };
 
     setUser(userData);
   };
 
-
   return (
     <div>
       <Search loadUser={loaderUser} />
-      {user && <User {...user}/>}
+      {user && <User {...user} />}
+      {error && <Error/>}
     </div>
   );
 };
